b7ff049c74b19a8447b1c4cbb0b05886c8bbdd9f
Edwin Neira <edwinchivata@habi.co>
2023-06-12 15:39:15 -0500

    add safari test case

--- src/test/java/SafariTest.java
+++ src/test/java/SafariTest.java
@@ -2,14 +2,12 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import java.net.MalformedURLException;
-
 public class SafariTest {
 
     @Test
     public void webDriverMacInit() {
         WebDriver driverSafari = new SafariDriver();
-        driverSafari.get("https://selenium.dev");
+        driverSafari.navigate().to("https://selenium.dev");
         driverSafari.quit();
     }
 
